452af98eef7dd7e7a4064b146b92842c91ecfe0f
Tamim-Amin <tamimamin13579@gmail.com>
2025-12-16 14:28:11 +0600

    add booking controller method

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Booking;
+use Illuminate\Support\Facades\Auth;
 
 class BookingController extends Controller
 {
-    //
+
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'provider_id' => 'required|exists:providers,id',
+            'problem_description' => 'required|string',
+            'service_date' => 'required|date|after_or_equal:today',
+            'service_time' => 'required',
+        ]);
+
+        Booking::create([
+            'customer_id' => Auth::id(),
+            'provider_id' => $validated['provider_id'],
+            'problem_description' => $validated['problem_description'],
+            'service_date' => $validated['service_date'],
+            'service_time' => $validated['service_time'],
+        ]);
+
+        return redirect()->back()->with('success', 'Booking created!');
+    }
 }
