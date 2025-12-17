6135211e89e196bc3d0d83d268c2ee4d09c21ecb
Tamim-Amin <tamimamin13579@gmail.com>
2025-12-17 13:56:11 +0600

    edit admin seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,16 +2,26 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Models\UserRole;
 
 class AdminSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $admin = User::create([
+            'name' => 'Tamim Amin',
+            'email' => 'tamimamin@gmail.com',
+            'password' => Hash::make('tamim102239'),
+        ]);
+
+        UserRole::create([
+            'user_id' => $admin->id,
+            'role' => 'admin',
+        ]);
+
+        echo "Admin created: tamimamin@gmail.com / tamim102239\n";
     }
 }
